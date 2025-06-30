4a883f7e68f704c167655d751643a0fb0677ecf1
KMCooper357 <155506008+KMCooper357@users.noreply.github.com>
2025-06-29 23:32:26 -0500

    changes

--- ec2create.py
+++ ec2create.py
@@ -6,13 +6,13 @@ ec2 = boto3.client('ec2')
 
 def create_instances():
     instances = ec2.run_instances(
-        ImageId='ami-0953476d60561c955, ami-084568db4383264d4',  # Replace your AMI ID 
+        ImageId='',  # Replace your AMI ID 
         InstanceType='t2.micro',  # instance type is the free tier
         MinCount=3,
         MaxCount=3,
-        KeyName='lambdaproject2025',  # Input a key pair name
-        SecurityGroupIds=['sg-001f91dc415319676'],  # Replace with your security group ID
-        SubnetId='subnet-0f90bb8daa333f236'  # Replace it with your subnet ID
+        KeyName='',  # Input a key pair name
+        SecurityGroupIds=[''],  # Replace with your security group ID
+        SubnetId=''  # Replace it with your subnet ID
     )
 
     for instance in instances['Instances']:
