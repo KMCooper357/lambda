21d353c59ddf79a7d8bda49a09e46bc337becdd7
KMCooper357 <155506008+KMCooper357@users.noreply.github.com>
2025-06-29 20:27:54 -0500

    change

--- instancesstopped.py
+++ instancesstopped.py
@@ -5,10 +5,28 @@ def lambda_handler(event, context):
     region = 'us-east-1'
     instance_ids = ['ami-0953476d60561c955', 'ami-084568db4383264d4']  # instance ID 
     
-    ec2.stop_instances(InstanceIds=instance_ids)
-    print(f'Stopping the instances!!!!!: {instance_ids}')
-    
-    return {
-        'statusCode': 200,
-        'body': f'Successfully stopped instances: {instance_ids}'
-    }
+    response = ec2.describe_instance( 
+        Filters=[
+            {
+                'Name': 'instance-state-name',
+                'Values': ['running']
+
+            }
+        ]
+    )
+
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_ids.append(instance['InstanceId'])
+
+    if instance_ids:
+        ec2.stop_instances(InstanceIds=Instance_ids)
+        return {
+            'statusCode': 200,
+            'body': f'Stopped instances: {Instance_ids}'
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': 'No running instances found.'
+        }
